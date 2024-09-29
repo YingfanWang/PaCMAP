be57e5b463d707dea29c99b674aba6ea8c73b297
“hyhuang00” <haiyanghuang99@gmail.com>
2024-09-29 10:17:27 -0500

    update demo files

--- demo/demo_utils.py
+++ demo/demo_utils.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+
 def generate_figure(embedding, labels, title):
     fig, ax = plt.subplots(1, 1, figsize=(6, 6))
     ax.scatter(embedding[:, 0], embedding[:, 1], s=0.5, c=labels, cmap='Spectral')
@@ -7,12 +8,13 @@ def generate_figure(embedding, labels, title):
     ax.set_title(title)
     plt.savefig(f"./{title}.png")
 
+
 def generate_combined_figure(embeddings, labels, titles, theme_title):
     len_subfigs = len(embeddings)
     assert len(labels) == len_subfigs
     assert len(titles) == len_subfigs
     n_rows = (len_subfigs + 2) // 3
-    fig, axes = plt.subplots(n_rows, 3, figsize = (18, n_rows * 6))
+    fig, axes = plt.subplots(n_rows, 3, figsize=(18, n_rows * 6))
     axes = axes.flatten()
     for i in range(len_subfigs):
         ax = axes[i]
